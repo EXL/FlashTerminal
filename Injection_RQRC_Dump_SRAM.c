673f1cbe09a761f57ca91ffb773be88486703bbf
EXL <exlmotodev@gmail.com>
2024-05-27 13:24:53 +0700

    Implement proper NOR loader for A830/U10

--- Injection_RQRC_Dump_SRAM.c
+++ Injection_RQRC_Dump_SRAM.c
@@ -2,6 +2,10 @@
 $ tcc -apcs /interwork -O2 Injection_RQRC_Dump_RAM.c -c -o patch_ram.o
 $ armlink -ro-base 0x0013B430 patch_ram.o V9m_RAMDLD_01B5.sym -o patch_ram.elf
 $ fromelf patch_ram.elf -bin -output patch_ram.bin
+
+$ mcore-elf-gcc -nostdinc -nostdlib -fshort-wchar -funsigned-char -fpic -fpie -mbig-endian -m340 \
+	-DM_CORE_MOTOROLA_A830_SIEMENS_U10 -O2 -c Injection_RQRC_Dump_SRAM.c -o Injection_RQRC_Dump_SRAM.o
+$ mcore-elf-ld -nostdinc -nostdlib -Bstatic Injection_RQRC_Dump_SRAM.o -TInjection_MCORE_M340.ld -o injection.bin
 #endif
 
 #define ADDR_CMD_ADDR_SIZE              8
@@ -16,6 +20,15 @@ typedef struct {
 	UINT32                       end_addr;
 } BLOADER_SECTION_ADDR_TBL;
 
+#if defined(M_CORE_MOTOROLA_A830_SIEMENS_U10)
+#define rsrc_str ((UINT8 *) 0x078064CB)
+#define blvar_RAM_section_addr_tbl (*(BLOADER_SECTION_ADDR_TBL *) 0x0782AD7C)
+
+#define HAPI_WATCHDOG_service ((void (*)(void)) (0x07803136))
+#define parser_send_packet ((void (*)(UINT8 *, UINT8 *)) (0x07800A7A))
+#define util_ui8_to_hexasc ((void (*)(UINT8, UINT8 *)) (0x078006C8))
+#define util_hexasc_to_ui32 ((UINT32 (*)(UINT8 *, UINT8)) (0x078006A0))
+#else
 extern UINT8 rsrc_str[];
 extern BLOADER_SECTION_ADDR_TBL blvar_RAM_section_addr_tbl;
 
@@ -23,6 +36,7 @@ extern void HAPI_WATCHDOG_service(void);
 extern void parser_send_packet(UINT8 *command_ptr, UINT8 *data_ptr);
 extern void util_ui8_to_hexasc(UINT8 val, UINT8 *str_ptr);
 extern UINT32 util_hexasc_to_ui32(UINT8 *str_ptr, UINT8 size);
+#endif
 
 extern void handle_command_RQRC(UINT8 *data_ptr) {
 	UINT8 response[MAX_RESPONSE_DATA_SIZE];
@@ -44,5 +58,9 @@ extern void handle_command_RQRC(UINT8 *data_ptr) {
 		HAPI_WATCHDOG_service();
 	}
 
+#if defined(M_CORE_MOTOROLA_A830_SIEMENS_U10)
+	parser_send_packet((UINT8 *) rsrc_str, response);
+#else
 	parser_send_packet((UINT8 *) &rsrc_str[0], response);
+#endif
 }
