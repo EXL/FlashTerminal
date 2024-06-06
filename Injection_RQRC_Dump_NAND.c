0d070f4d76c3a0c681863848877b5595dcf054d2
EXL <exlmotodev@gmail.com>
2024-06-07 03:08:59 +0700

    V3m/MSM6500: Phase 14

--- Injection_RQRC_Dump_NAND.c
+++ Injection_RQRC_Dump_NAND.c
@@ -128,10 +128,9 @@ void handle_command_RQRC(UINT8 *data_ptr) {
 		}
 	}
 #elif defined(FTR_V3M_MSM6500)
-	if (blvar_RAM_section_addr_tbl.start_addr == 0x64000000) {
 		*((UINT32 *) 0x80000904) = 0x2000;
 		// 0x031C / NAND_FLASH_CFG1, 0 bit - ECC_DISABLE
-		*((UINT32 *) 0x6400031C) = *((UINT32 *) 0x6400031C) & 0xFFFFFFFE /*| 1*/;
+		*((UINT32 *) 0x6400031C) = *((UINT32 *) 0x6400031C) & 0xFFFFFFFE | 1;
 
 		*((UINT32 *) 0x64000304) = page << 9; // 0x0304 / NAND_FLASH_ADDR, 31:9 bits - NAND_FLASH_PAGE_ADDRESS
 
@@ -139,7 +138,11 @@ void handle_command_RQRC(UINT8 *data_ptr) {
 		*((UINT32 *) 0x64000300) = 1;         // 0x0300 / NAND_FLASH_CMD, 2:0 bits - OP_CMD, 001 - page_read
 
 		watchdog_check_delay_110344();
-	}
+
+		*((UINT32 *) 0x80000904) = 0x2000;
+		*((UINT32 *) 0x6400031C) = *((UINT32 *) 0x6400031C) & 0xFFFFFFFE;
+
+		watchdog_check_delay_110344();
 #else
 	#error "Unknown device or unknown MSM SoC!"
 #endif
