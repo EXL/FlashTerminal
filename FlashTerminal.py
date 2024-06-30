8452184254906ff7c54cff9618b89ee0c7dc0b3a
EXL <exlmotodev@gmail.com>
2024-06-30 13:13:33 +0700

    V60/Patriot SoC: Phase 4

--- FlashTerminal.py
+++ FlashTerminal.py
@@ -130,6 +130,7 @@ def worksheet(er, ew):
 #	mfp_dump_sram_1byte(er, ew, 'V60_ROM_Dump.bin', 0x10000000, 0x10400000)
 	mfp_dump_sram(er, ew, 'V60_IROM_Dump.bin', 0x00000000, 0x00400000, 0x30)
 #	mfp_dump_sram(er, ew, 'V60_ROM_Dump.bin', 0x10000000, 0x10400000, 0x30)
+	mfp_dump_sram(er, ew, 'V66i_ROM_Dump.bin', 0x10000000, 0x10800000, 0x30)
 	# ~~~~~~~~~~~~~~~~~~~~ UNDER CONSTRUCT ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
 
 	# Motorola A835/A845 dumping tricks.
