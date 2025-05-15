2b319b7cf0f25fce89bd049c2cc951942e268246
EXL <exlmotodev@gmail.com>
2025-05-15 18:01:40 +0700

    V980/PoG: Phase 2

--- FlashTerminal.py
+++ FlashTerminal.py
@@ -139,7 +139,7 @@ def worksheet(er, ew):
 #		mfp_upload_binary_to_addr(er, ew, 'loaders/E398_RAMDLD_07B0_Hacked_Dump.ldr', 0x03FD0000, 0x03FD0010)
 #		mfp_upload_binary_to_addr(er, ew, 'loaders/L6_RAMDLD_08D5_RSA_Read.ldr', 0x03FD0000, 0x03FD0010, True)
 #		mfp_upload_binary_to_addr(er, ew, 'loaders/E1000_RAMDLD_0610.ldr', 0x07804000, 0x07804010, True)
-		mfp_upload_binary_to_addr(er, ew, 'loaders/V980_32M_RAMDLD_0636.ldr', 0x07804000, 0x07804010, True)
+		mfp_upload_binary_to_addr(er, ew, 'loaders/V980_32M_RAMDLD_0636.ldr', 0x08000000, 0x08000010, True)
 #		mfp_upload_binary_to_addr(er, ew, 'loaders/V3x_RAMDLD_0682_RSA_Read.ldr', 0x08000000, 0x08000010, True)
 #		mfp_upload_binary_to_addr(er, ew, 'loaders/A1000_BP_RAMDLD_0651_RSA_Read.ldr', 0x08000000, 0x08000010, True)
 #		mfp_upload_binary_to_addr(er, ew, 'loaders/A835_RAMDLD_0612_Hacked_RSA_Read.ldr', 0x08000000, 0x08018818)
