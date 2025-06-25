2b9e108e08e6068661e7e75b36dbd5cec3f5ad70
EXL <exlmotodev@gmail.com>
2025-06-25 23:00:37 +0700

    CC75 03

--- FlashTerminal.py
+++ FlashTerminal.py
@@ -254,12 +254,12 @@ def worksheet(er, ew):
 
 	# Siemens CC75 on Spansion flash memory chip flashing!
 #	buffer_write_size = 0x40
+	mfp_cmd(er, ew, 'ERASE')
 #	mfp_cmd(er, ew, 'ERASE')
 #	mfp_cmd(er, ew, 'ERASE')
-#	mfp_cmd(er, ew, 'ERASE')
-#	mfp_upload_binary_to_addr(er, ew, 'CC75_Chunk.bin', 0x100C0000, None)
+	mfp_upload_binary_to_addr(er, ew, 'CC75_Chunk.bin', 0x100C0000, None)
 
-	mfp_dump_read(er, ew, 'READ_0.bin', 0x10080000, 0x10300000, 0x100)
+#	mfp_dump_read(er, ew, 'READ_1.bin', 0x10080000, 0x10300000, 0x100)
 
 #	mfp_upload_binary_to_addr(er, ew, 'CC75_PBlocks.bin', 0x11FE0000, None)
 
