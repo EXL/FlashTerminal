94199a244d440694e58d52c0a0f2b0509c649fb9
EXL <exlmotodev@gmail.com>
2025-09-05 01:21:43 +0700

    Z6c 11

--- FlashTerminal.py
+++ FlashTerminal.py
@@ -262,7 +262,7 @@ def worksheet(er, ew):
 	# Dump NAND data (64 MiB / 128 MiB / 256 MiB) and spare area.
 	# Chunks are 528 bytes == 512 bytes is NAND page size + 16 bytes is NAND spare area.
 #	mfp_dump_nand(er, ew, 'Z6m_NAND_Dump.bin', 0, int(0x04000000 / 512), 0x30)
-	mfp_dump_nand(er, ew, 'Z6c_NAND_Dump_6.bin', 131072, int(0x08001000 / 512), 0x30)
+	mfp_dump_nand(er, ew, 'Z6c_NAND_Dump_6.bin', 131072 + 103900, int(0x08001000 / 512), 0x30)
 #	mfp_dump_nand(er, ew, 'V9m_NAND_Dump.bin', 0, int(0x08000000 / 512), 0x30)
 #	mfp_dump_nand(er, ew, 'VE40_NAND_Dump.bin', 0, int(0x08000000 / 512), 0x10)
 #	mfp_dump_nand(er, ew, 'ic902_NAND_Dump.bin', 0, int(0x08000000 / 512), 0x10)
