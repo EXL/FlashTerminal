5f4f46738e18ebeb1792ec55348b8842742b941a
EXL <exlmotodev@gmail.com>
2026-06-19 10:15:47 +0700

    V120t/Patriot: 2

--- FlashTerminal.py
+++ FlashTerminal.py
@@ -236,7 +236,7 @@ def worksheet(er, ew):
 #	mfp_dump_dump(er, ew, 'C350_ROM_Dump.bin', 0x00000000, 0x00800000, 0x100)
 #	mfp_dump_dump(er, ew, 'C350_IROM_Dump.bin', 0x10000000, 0x10400000, 0x100)
 #	mfp_dump_dump(er, ew, 'C550_ROM_Dump.bin', 0x00000000, 0x01000000, 0x100)
-	mfp_dump_sram(er, ew, 'V60_ROM_Dump.bin', 0x10000000, 0x10400000, 0x30)
+	mfp_dump_sram(er, ew, 'V60_ROM_Dump_2.bin', 0x10132000, 0x10401000, 0x30)
 #	mfp_dump_sram(er, ew, 'V60_IROM_Dump.bin', 0x00000000, 0x00400000, 0x30)
 #	mfp_dump_sram(er, ew, 'V70_ROM_Dump.bin', 0x10000000, 0x10800000, 0x30)
 #	mfp_dump_sram(er, ew, 'V120e_ROM_Dump.bin', 0x00000000, 0x00500000, 0x30) # 4 MiB + 1 MiB
